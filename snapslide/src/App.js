dd5fcc171aa941e937fdffb1e5f50e554f332b30
shikhu51197 <sg780060@gmail.com>
2023-09-22 02:11:01 +0530

    add drag

--- snapslide/src/App.js
+++ snapslide/src/App.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React ,{ useState } from 'react';
 import './App.css';
 import BottomSheet from './BottomSheet';
 
+const bodyStyle = {
+  width: "100%",
+  height: "100%",
+  backgroundColor: "rgba(7, 0, 0, 0.18)",
+};
+
+
 function App() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handle_modal = (a) => {
+    setIsModalOpen(a);
+  };
   return (
-    <div className="App">
+    <div className="App" style={isModalOpen ? bodyStyle : {}}>
       <header className="App-header">
         <h1>React Bottom Sheet</h1>
       </header>
-      <main>
-        <BottomSheet />
-      </main>
+     
+        <BottomSheet handle_modal={handle_modal} />
+      
     </div>
   );
 }
--- snapslide/src/BottomSheet.jsx
+++ snapslide/src/BottomSheet.jsx
@@ -1,50 +1,132 @@
-import React, { useState } from 'react';
-import './BottomSheet.css';
+import { useState, useRef, useEffect } from "react";
+import "./BottomSheet.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const BottomSheet = () => {
-  const [snapPoint, setSnapPoint] = useState('closed');
-  const [showFullViewButton, setShowFullViewButton] = useState(true);
 
-  const toggleBottomSheet = () => {
-    if (snapPoint === 'closed') {
-      setSnapPoint('half-open');
-    } else if (snapPoint === 'half-open') {
-      setSnapPoint('open');
-      setShowFullViewButton(false); 
+
+const BottomSheet = ({ handle_modal }) => {
+  const [position, setPosition] = useState("closed");
+  const [isDragging, setIsDragging] = useState(false);
+  let initialY = useRef(null);
+  let delay = 0;
+  let prev = 0;
+  const sheetRef = useRef(null);
+  const contentRef = useRef(null);
+
+  useEffect(() => {
+    if (position === "closed") {
+      // Return focus to the element that triggered opening the sheet
+      contentRef.current?.focus();
     } else {
-      setSnapPoint('closed');
-      setShowFullViewButton(true); 
+      // Focus the content inside the sheet
+      sheetRef.current?.focus();
+    }
+  }, [position]);
+
+  const handleSnap = (newPosition) => {
+    if (newPosition !== position) {
+      handle_modal(newPosition === "fully-open");
+      setPosition(newPosition);
+    }
+  };
+
+  const handleMouseDown = (e) => {
+    setIsDragging(true);
+    initialY.current = e.pageY || e.touches?.[0].pageY;
+    prev = e.pageY || e.touches?.[0].pageY;
+  };
+
+  const handleMouseMove = (e) => {
+    if (isDragging) {
+      delay = initialY.current - (e.pageY || e.touches?.[0].pageY);
+      prev = e.pageY || e.touches?.[0].pageY;
+    }
+  };
+
+  const handleMouseUp = () => {
+    if (delay === prev) return;
+    if (delay >= 0 && position === "closed") {
+      handleSnap("half-open");
+    } else if (delay > 0 && position === "half-open") {
+      handleSnap("fully-open");
+    } else if (delay < 0 && position === "fully-open") {
+      handleSnap("half-open");
+    } else if (delay < 0 && position === "half-open") {
+      handleSnap("closed");
+    }
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Escape") {
+      // Close the sheet when the "Escape" key is pressed
+      handleSnap("closed");
+    } else if (e.key === "Tab" && position !== "closed") {
+      // Manage focus within the sheet when it's open
+      e.preventDefault();
+      const focusableElements = sheetRef.current.querySelectorAll(
+        "button, [href], input, select, textarea, [tabindex]:not([tabindex='-1'])"
+      );
+      const firstFocusable = focusableElements[0];
+      const lastFocusable = focusableElements[focusableElements.length - 1];
+      if (e.shiftKey) {
+        if (document.activeElement === firstFocusable) {
+          lastFocusable.focus();
+        } else {
+          const index = Array.from(focusableElements).indexOf(
+            document.activeElement
+          );
+          focusableElements[index - 1].focus();
+        }
+      } else {
+        if (document.activeElement === lastFocusable) {
+          firstFocusable.focus();
+        } else {
+          const index = Array.from(focusableElements).indexOf(
+            document.activeElement
+          );
+          focusableElements[index + 1].focus();
+        }
+      }
     }
   };
 
   return (
-    <div className={`bottom-sheet ${snapPoint}`} onClick={toggleBottomSheet}>
-      <div className="handle"></div>
-      <div className="content">
-        {snapPoint === 'closed' && <button>Click to Open</button>}
-        {snapPoint !== 'closed' && (
-          <>
-            <h2>Bottom Sheet Content</h2>
-            <p>
-              content..... if the header or footer changes their height the sheet will readjust accordingly.
-              <br /> <br /><br />
-              {showFullViewButton && <button>Full View</button>}
+
+    <div
+      ref={sheetRef}
+      className={`bottom-sheet ${position} `}
+      onMouseDown={handleMouseDown}
+      onMouseMove={handleMouseMove}
+      onMouseUp={handleMouseUp}
+      onKeyDown={handleKeyDown}
+      tabIndex={position === "closed" ? -1 : 0}
+    >
+
+
+
+      <div className="content" tabIndex={-1} ref={contentRef}>
+     🔼⏫⬆️
+      <h2>Bottom Sheet Content</h2>
+      <p>
+              Just as with content, if the header or footer changes their height, the sheet will readjust accordingly.
             </p>
-
-            {snapPoint === 'open' && (
-              <>
-              
-                <p>
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit ? here...
-                  <br></br>
-                  <button onClick={() => setShowFullViewButton(true)}>close</button>
+            <br/>  
+            <p>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias itaque veritatis accusamus?
+                  Laboriosam, iure dolor. Beatae voluptatum accusamus harum ipsa quidem, soluta velit voluptates
+                  voluptatibus numquam minima at saepe illo?here...
                 </p>
-
-                <p>More content...</p>
-              </>
-            )}
-          </>
-        )}
+         
+      </div>
+      <div className="handle" tabIndex={-1}></div>
+      <div className="controls">
+        <button
+          style={{ marginRight: "5px" }}
+          onClick={() => handleSnap("fully-open")}
+        >
+          Full open view
+        </button>
+        <button onClick={() => handleSnap("closed")}>Full close view</button>
       </div>
     </div>
   );
